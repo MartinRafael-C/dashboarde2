6842843330f65f77b59549e563903ae84debfb56
contrerasquizhpimartin <contrerasquizhpimartin@gmail.com>
2025-09-17 16:47:18 +0000

    ✨feat(app) Color de fondo

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,10 @@
-import { Text, View } from "react-native";
+import ProfileDetails from "@/components/ProfileDetails";
+import { View } from "react-native";
 import "../global.css";
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center bg-white px-6 py-8  "
-    >
-     
+    <View className="flex justify-center items-center bg-white w-screen h-screen">
+      <ProfileDetails></ProfileDetails>
     </View>
   );
 }
