be8f609980710b5c6e3fb52e350bd956f79a5046
contrerasquizhpimartin <contrerasquizhpimartin@gmail.com>
2025-09-17 15:55:59 +0000

    ✨feat(app):Instalación de nateivewind y estilos de prueba -Agregar metro config , babel.config en el comando -Crear hoja de estilos global.css -crear config con el comando  npx tailwindss init

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,10 @@
 import { Text, View } from "react-native";
-
+import "../global.css";
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <View className="flex-1 justify-center items-center bg-white px-6 py-8  "
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-white text-2xl font-bold">Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
