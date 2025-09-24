a8957f3433e227cea5a781f7cf51a04d40f40dc3
contrerasquizhpimartin <contrerasquizhpimartin@gmail.com>
2025-09-24 02:45:25 +0000

    feat✨(ProfileDetails) Importar Linking en ProfileDetails -Crear y organizar constant links -Añadir links en lis ìconos con linking.openUrl("") -Añadr foto de perfil

---
+++ components/IconButton.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Image, Text, Pressable } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+interface IconButtonProps {
+pulsar:() => void;
+icon: string;
+label: string;
+enlace?:string;
+
+}
+const IconButton = ({icon,pulsar,enlace}:IconButtonProps) => {
+  return (
+
+};
+
+export default IconButton;
--- components/ProfileDetails.tsx
+++ components/ProfileDetails.tsx
@@ -1,20 +1,28 @@
 import { View, Image, Text } from 'react-native'
 import React from 'react'
+import IconButton from "./IconButton"
 import "@/global.css"
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 
 
 const ProfileDetails = () => {
   return (
     <View className="flex justify-center items-center">
-      <Image 
-      className ="max-h-36  max-w-36 rounded-full "
-      source={require('../assets/images/icon.png')} 
+      <Image
+        className="max-h-36  max-w-36 rounded-full "
+        source={require('../assets/images/icon.png')}
       />
-     
+
       <Text className="text-2xl font-bold text-center"> Martin Contreras</Text>
       <Text className="text-xl font-blod text-gray-400">Student </Text>
 
-      
+
+      <IconButton icon='logo-instagram' pulsar={() => console.log("Instagram")} label={''}  ></IconButton>
+      <IconButton icon='facebook-square' pulsar={() => console.log("Facebook")} label={''} ></IconButton>
+      <IconButton icon='whatsapp' pulsar={() => console.log("Whatsaap")} label={''} ></IconButton>
+      <IconButton icon='linkedin' pulsar={() => console.log("Linkedin")} label={''} ></IconButton>
+
+
 
 
     </View>
